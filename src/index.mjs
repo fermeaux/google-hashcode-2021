2aee1538574bfc18036dc727d9b86356dacfc1f9
Timothée Fermeaux <fermeaux.t@gmail.com>
2021-02-24 20:48:39 +0100

    chore: read inputs

--- src/index.mjs
+++ src/index.mjs
@@ -3,7 +3,12 @@ import { read } from './reader.mjs'
 import { resolve } from './resolver.mjs'
 import { write } from './writer.mjs'
 
+console.log('===== START PROGRAM =====')
 const inputs = read()
+console.log('INPUTS : ', inputs)
 const datas = parse(inputs)
+console.log('DATAS : ', datas)
 const solutions = resolve(datas)
+console.log('SOLUTIONS : ', datas)
 write(solutions)
+console.log('===== END PROGRAM =====')
--- src/reader.mjs
+++ src/reader.mjs
@@ -1,1 +1,10 @@
-export function read () {}
+import fs from 'fs'
+
+export function read () {
+  const fileNames = fs.readdirSync('inputs').filter(fileName => fileName.endsWith('.in'))
+  const inputs = fileNames.map(fileName => ({
+    fileName: fileName.split('.')[0],
+    content: fs.readFileSync(`inputs/${fileName}`).toString().trim().replace(/\r/g, '').split('\n')
+  }))
+  return inputs
+}
