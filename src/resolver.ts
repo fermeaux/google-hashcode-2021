548819e32059783a6c5a99d6c5e50048a9f34a86
Victor Voisin <victor.voisin@unicoda.fr>
2021-02-25 20:13:38 +0100

    Add function computeOptimalPathTime

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,5 +1,15 @@
-import { Data, Solution } from './models'
+import { Car, Data, Solution } from './models'
 
 export function resolve (data: Data): Solution {
   return {}
+}
+
+const computeOptimalPathTime = (car: Car): number => {
+  let cost = 0;
+
+  for(const street of car.route) {
+    cost += street.duration;
+  }
+
+  return cost;
 }
